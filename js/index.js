6ae9d07d4df51e90c2d354cd5c039a38fc0abbcb
Your Name <you@example.com>
2019-12-11 16:32:55 -0500

    dger

--- js/index.js
+++ js/index.js
@@ -5,7 +5,24 @@ setTimeout(function() {
 },0)
 
 
+$(".hero-img-container").mousemove(function(e) {
+  // parallaxIt(e, ".abso0", -8);
+  // parallaxIt(e, ".abso00", 3);
+  parallaxIt(e, ".abso4", -8);
+  parallaxIt(e, ".abso7", 8);
 
+});
+
+function parallaxIt(e, target, movement) {
+  var $this = $(".hero-img-container");
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width() / 2) / $this.width() * movement,
+    y: (relY - $this.height() / 2) / $this.height() * movement
+  });
+}
 
 
 })
