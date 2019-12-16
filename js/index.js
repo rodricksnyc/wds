7a19310bd456df5d27526a56a8807ce89d2b3a78
Your Name <you@example.com>
2019-12-16 12:13:58 -0500

    dger

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 
-$("#first").on('mouseenter' , function() {
+$("#first").mouseenter(function() {
 
   setTimeout(function() {
     $('.first').addClass('animated slideOutUp')
@@ -27,67 +27,65 @@ setTimeout(function() {
 
 })
 
+$(".second", ".third", ".fourth").mouseenter(function() {
+    setTimeout(function() {
+      $('.first').removeClass('animated slideOutUp').addClass('animated slideInDown')
+    },0)
+
+
+  setTimeout(function() {
+    $('#clicked0 .box').addClass('transparent').removeClass('animated slideInUp');
+    $('#clicked2 .box').addClass('transparent').removeClass('animated flipInX');
+    $('#clicked3 .box').addClass('transparent').removeClass('animated flipInX');
+    $('#clicked4 .box').addClass('transparent').removeClass('animated tada');
+
+  },300)
+})
+
+
+
 // $("#second").mouseenter(function() {
 //
+//
 //   setTimeout(function() {
-//     $('.first').removeClass('animated slideOutUp').addClass('animated slideInDown')
+//     $('.second').addClass('animated slideOutUp')
 //   },0)
 //
+//   setTimeout(function() {
+//     $('#clicked66 .box').removeClass('transparent').addClass('animated slide-both infinite slow')
+//   },500)
 //
-// setTimeout(function() {
-//   $('#clicked0 .box').addClass('transparent').removeClass('animated slideInUp');
-//   $('#clicked2 .box').addClass('transparent').removeClass('animated flipInX');
-//   $('#clicked3 .box').addClass('transparent').removeClass('animated flipInX');
-//   $('#clicked4 .box').addClass('transparent').removeClass('animated tada');
+// // setTimeout(function() {
+// // const slideFade = (elem) => {
+// //    const fade = { opacity: 0, transition: 'opacity .5s' };
+// //    elem.css(fade).addClass('animated slideOutUp slower')
+// //    }
+// //
+// // slideFade($('.abso44'));
+// // },1900)
+// //
+// // setTimeout(function() {
+// //   $('.abso444').removeClass('transparent').addClass('animated fadeInUp')
+// //
+// // },1980)
 //
-// },300)
+// $("#all").mousemove(function(e) {
+//   parallaxIt(e, ".phone", 15);
 //
+// });
+//
+// function parallaxIt(e, target, movement) {
+//   var $this = $("#all");
+//   var relX = e.pageX - $this.offset().left;
+//   var relY = e.pageY - $this.offset().top;
+//
+//   TweenMax.to(target, 1, {
+//     x: (relX - $this.width() / 2) / $this.width() * movement,
+//     y: (relY - $this.height() / 2) / $this.height() * movement
+//   });
+// }
 //
 // })
-
-
-$("#second").mouseenter(function() {
-
-
-  setTimeout(function() {
-    $('.second').addClass('animated slideOutUp')
-  },0)
-
-  setTimeout(function() {
-    $('#clicked66 .box').removeClass('transparent').addClass('animated slide-both infinite slow')
-  },500)
-
-// setTimeout(function() {
-// const slideFade = (elem) => {
-//    const fade = { opacity: 0, transition: 'opacity .5s' };
-//    elem.css(fade).addClass('animated slideOutUp slower')
-//    }
-//
-// slideFade($('.abso44'));
-// },1900)
-//
-// setTimeout(function() {
-//   $('.abso444').removeClass('transparent').addClass('animated fadeInUp')
-//
-// },1980)
-
-$("#all").mousemove(function(e) {
-  parallaxIt(e, ".phone", 15);
-
-});
-
-function parallaxIt(e, target, movement) {
-  var $this = $("#all");
-  var relX = e.pageX - $this.offset().left;
-  var relY = e.pageY - $this.offset().top;
-
-  TweenMax.to(target, 1, {
-    x: (relX - $this.width() / 2) / $this.width() * movement,
-    y: (relY - $this.height() / 2) / $this.height() * movement
-  });
-}
-
-})
 
 
 
