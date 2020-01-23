eede381c4cde491b963fe95141b5de5e65de1e69
Your Name <you@example.com>
2020-01-23 11:14:45 -0500

    changes

--- js/index.js
+++ js/index.js
@@ -200,6 +200,49 @@ if( target.length ) {
 })
 
 
+
+$(window).on('scroll', function() {
+var screenTop = $(window).scrollTop();
+var screenBottom = $(window).scrollTop() + window.innerHeight;
+
+$('section').each(function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = $(this).offset().top + $(this).outerHeight();
+
+    if ((screenBottom > elementTop + ($(this).find('.heros').height() / 7)) && (screenTop < elementBottom)) {
+        $('section').removeClass('active')
+        $(this).addClass('active')
+    } else {
+        $(this).removeClass('active')
+    }
+})
+
+if ($('#process').hasClass('active')) {
+
+setTimeout(function() {
+$('section .fixed-container').css('position', 'fixed')
+
+},2000)
+
+}
+
+else {
+  $('section .fixed-container').css('position', 'sticky')
+}
+
+})
+
+// $(document).scroll(function() {
+//     var y = $(document).scrollTop(), //get page y value
+//         header = $("#process-container"); // your div id
+//     if(y >= 2000)  {
+//         header.css({position: "fixed", "top" : "0", "left" : "0"});
+//     } else {
+//         header.css("position", "sticky");
+//     }
+// });
+
+
   $(".hover1").bind('mouseenter', function() {
 
     if (window.innerHeight < 900) {
